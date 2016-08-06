06211cc4eeffcc341b8e41f068164b60da37454a
Kyle <kyyjoyce@gmail.com>
2016-08-05 20:38:17 -0600

    working and styled, adding more imgs

--- photoQuote.js
+++ photoQuote.js
@@ -1,35 +1,34 @@
 
-$( document ).ready(function() {  
+$(document).ready(function () {
     getQuote();
 });
 
 
 /* This function retrieves a quote from the Forismatic API and applies it to the appropriate divs */
 
-function getQuote() {    
+function getQuote() {
     var request = $.ajax({
         url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
-        dataType: "jsonp"                
+        dataType: "jsonp"
     });
 
-    request.done(function(data) {
+    request.done(function (data) {
         $("#log").html(data);
         $("#quoteText").text(data.quoteText);
         $("#quoteAuthor").text(data.quoteAuthor);
     });
 
-    request.fail(function(jqXHR, textStatus) {
+    request.fail(function (jqXHR, textStatus) {
         alert("Request failed: " + textStatus);
     })
 };
 
 /* This function changes the background image */
 
-function randomImg(){
+function randomImg() {
 
     var photos = ['backImg-1.jpg', 'backImg-2.jpg', 'backImg-3.jpg', 'backImg-4.jpg', 'backImg-5.jpg', 'backImg-6.jpg'];
 
-    $('html').css({'background': 'url(photos/' + photos[Math.floor(Math.random() * photos.length)] + ')'});
-    $('html').css({'background-size': 'cover'});
-//Test
+    $('html').css({ 'background': 'url(photos/' + photos[Math.floor(Math.random() * photos.length)] + ')' });
+    $('html').css({ 'background-size': 'cover' });
 };
