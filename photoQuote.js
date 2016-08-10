443cebf75506904c8055516a5aef5f706220b504
Kyle <kyyjoyce@gmail.com>
2016-08-10 00:04:32 -0600

    image div instead of html

--- photoQuote.js
+++ photoQuote.js
@@ -1,16 +1,20 @@
+//"use strict";
+var photos = null;
 
 $(document).ready(function () {
+    preCachePhotos();
+    
     randomImg();
-   $('html').css('transition', 'all 0.5s ease');
+       
     getQuote();
 });
 
-
-/* This function retrieves a quote from the Forismatic API and applies it to the appropriate divs */
-
+/*
+ * This function retrieves a quote from the Forismatic API and applies it to the appropriate divs
+ */
 function getQuote() {
     var request = $.ajax({
-        url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+        url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
         dataType: "jsonp"
     });
 
@@ -25,35 +29,28 @@ function getQuote() {
     })
 };
 
-/* This function changes the background image by calculating a random number and accessing the photo at that number */
-
+/*
+ * This function changes the background image by calculating a random number and accessing the photo at that number
+ */
+// pull two images at first rather than one
+// put in object, one key value pair (current image), another key value pair (next image)
 function randomImg() {
 
-    var photos = ['landscape1.jpg', 'landscape2.jpg', 'landscape3.jpg', 'landscape4.jpg', 'landscape5.jpg', 'landscape6.jpg', 'landscape7.jpg', 'landscape8.jpg', 'landscape9.jpg', 'landscape10.jpg', 'landscape11.jpg', 'landscape12.jpg', 'landscape13.jpg', 'landscape14.jpg', 'landscape15.jpg', 'landscape16.jpg', 'landscape17.jpg', 'landscape18.jpg', 'landscape19.jpg', 'landscape20.jpg', 'landscape21.jpg', 'landscape22.jpg', 'landscape23.jpg', 'landscape24.jpg', 'landscape25.jpg', 'landscape26.jpg', 'landscape27.jpg', 'landscape28.jpg', 'landscape29.jpg', 'landscape30.jpg', 'landscape31.jpg', 'landscape32.jpg', 'landscape33.jpg', 'landscape34.jpg', 'landscape35.jpg', 'landscape36.jpg', 'landscape37.jpg']
+    photos = ['landscape1.jpg', 'landscape2.jpg', 'landscape3.jpg', 'landscape4.jpg', 'landscape5.jpg', 'landscape6.jpg', 'landscape7.jpg', 'landscape8.jpg', 'landscape9.jpg', 'landscape10.jpg', 'landscape11.jpg', 'landscape12.jpg', 'landscape13.jpg', 'landscape14.jpg', 'landscape15.jpg', 'landscape16.jpg', 'landscape17.jpg', 'landscape18.jpg', 'landscape19.jpg', 'landscape20.jpg', 'landscape21.jpg', 'landscape22.jpg', 'landscape23.jpg', 'landscape24.jpg', 'landscape25.jpg', 'landscape26.jpg', 'landscape27.jpg', 'landscape28.jpg', 'landscape29.jpg', 'landscape30.jpg', 'landscape31.jpg', 'landscape32.jpg', 'landscape33.jpg', 'landscape34.jpg', 'landscape35.jpg', 'landscape36.jpg', 'landscape37.jpg']
 
-    var responsiveImg = { 
-
-        'background-size': 'cover', 
-        'background-repeat': 'no-repeat', 
-        'background-attachment': 'fixed',
-        'transition': 'all 0.5s ease'
-    }; 
-
-    $('html').css({ 'background': 'url(photos/' + photos[Math.floor(Math.random() * photos.length)] + ')' });
-     $('html').css(responsiveImg);
+    $('#bgPic1').css({ 'background': 'url(photos/' + photos[Math.floor(Math.random() * photos.length)] + ')' });
 };
 
-// Begin loading image array when window loads
-
-function preCachePhotos(){
+/*
+ * Begin loading image array when window loads
+ */
+function preCachePhotos(){    
     $.each(photos, function(){
         var img = new Image();
         img.src = this;
     });
 };
  
-$(window).load(function(){
-    preCachePhotos();
-});
-
-
+//$(window).load(function(){
+//    preCachePhotos();
+//});
